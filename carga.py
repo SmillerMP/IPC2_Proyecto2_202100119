dc8bd76bc86aff8ad558190aba54958750af73f3
SmillerMP <samuelmp7@gmail.com>
2023-03-13 16:02:33 -0600

    Correcciones carga del archivo
    se corrigieron algunos errores en el archivo de carga, para que pueda leer cada uno de los elementos necesarios en  XML

--- carga.py
+++ carga.py
@@ -11,35 +11,44 @@ import xml.etree.ElementTree as ET
 
 
 def cargaArchivo():
-    ruta = "D:\\Samuellllll\\Documentos\\Universidad\\CUARTO SEMESTRE\\IPC 2\\Proyectos IPC2\\IPC2_Proyecto2_202100119\\prueba.xml"
+    ruta = "D:\\Samuellllll\\Documentos\\Universidad\\CUARTO SEMESTRE\\IPC 2\\Proyectos IPC2\\IPC2_Proyecto2_202100119\\IPC2_Proyecto2_202100119\\prueba.xml"
     tree = ET.parse(ruta)
     root = tree.getroot()
 
 
-    for elemento in root.findall(".//elemento"):
-        numAtomico = elemento.find("numeroAtomico").text
+    for elemento in root.findall(".//listaElementos/elemento"):
+        numeroAtomico = elemento.find("numeroAtomico").text
         simbolo = elemento.find("simbolo").text
         nombreElemento = elemento.find("nombreElemento").text
         
-        print(f"Numero Atomico: {numAtomico}, Simbolo: {simbolo}, Nombre Elemento: {nombreElemento}")
+        print(f"Numero Atomico: {numeroAtomico}, Simbolo: {simbolo}, Nombre Elemento: {nombreElemento}")
 
 
-    for Maquina in root.findall(".//Maquina"):
+    for Maquina in root.findall(".//listaMaquinas/Maquina"):
         nombre = Maquina.find("nombre").text
         numeroPines = Maquina.find("numeroPines").text
         numeroElementos = Maquina.find("numeroElementos").text
 
         print(f"Nombre: {nombre}, Numero Pines: {numeroPines}, Numero Elementos: {numeroElementos}")
 
+        for elementos in Maquina.findall("pin/elementos/elemento"):
+            elemento = elementos.text
 
-        for elementos in Maquina.findall("pin/elementos"):
-            elemento = elementos.find("elemento").text
             
             print(f"Elemento: {elemento}")
 
 
 
-    # for compuesto in root.findall(".//compuesto"):
-    #     nombre = compuesto.find("nombre")
+
+    for compuesto in root.findall(".//listaCompuestos/compuesto"):
+        nombre = compuesto.find("nombre").text
+
+
+        for elementos in compuesto.findall("elementos/elemento"):
+            elemento = elementos.text
+
+            print(f"Nombre: {nombre}, Elemento: {elemento}")
+
+            
 
 cargaArchivo()
