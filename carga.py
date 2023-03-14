183cc07c9f7427c9faa9e08863b4265156c6c4db
SmillerMP <samuelmp7@gmail.com>
2023-03-13 20:46:44 -0600

    Mejoras Carga XML
    se mejoraron algunas cosas de la carga del XML y se agrego el archivo Clase para guardar los datos

--- carga.py
+++ carga.py
@@ -15,39 +15,84 @@ def cargaArchivo():
     tree = ET.parse(ruta)
     root = tree.getroot()
 
-
-    for elemento in root.findall(".//listaElementos/elemento"):
-        numeroAtomico = elemento.find("numeroAtomico").text
-        simbolo = elemento.find("simbolo").text
-        nombreElemento = elemento.find("nombreElemento").text
-        
-        print(f"Numero Atomico: {numeroAtomico}, Simbolo: {simbolo}, Nombre Elemento: {nombreElemento}")
-
-
+    
+    contador = 0
+    ListaElemetosError = []
+    ListaPinError = []
     for Maquina in root.findall(".//listaMaquinas/Maquina"):
-        nombre = Maquina.find("nombre").text
+
+        # Comprobacion numero de Pines
         numeroPines = Maquina.find("numeroPines").text
-        numeroElementos = Maquina.find("numeroElementos").text
+        listaPines  = Maquina.findall("pin")
+        numeroPines = int(numeroPines)
 
-        print(f"Nombre: {nombre}, Numero Pines: {numeroPines}, Numero Elementos: {numeroElementos}")
+        # Comprobacion Numero de Elementos
+        cantidadElementos = Maquina.find("numeroElementos").text
+        listaElementos = Maquina.findall("pin/elementos/elemento")
+        cantidadElementos = int(cantidadElementos)
+        
+        contador += 1
 
-        for elementos in Maquina.findall("pin/elementos/elemento"):
-            elemento = elementos.text
+        #print(len(listaPines), numeroPines)
+    
 
+        if (len(listaPines) != numeroPines):
+            ListaPinError.append(contador)
+
+        #print(len(listaElementos), (cantidadElementos*numeroPines))
+
+        if (len(listaElementos) != (cantidadElementos*numeroPines)):
+            ListaElemetosError.append(contador)
+
+    #print(ListaElemetosError)
+
+
+
+    if (len(ListaPinError) != 0):
+        print("Existe Error con los PINES del XML en las Siguientes Maquinas: ")
+        for x in ListaPinError:
+            print(f"Maquina: {x}")
+
+    elif (len(ListaElemetosError) != 0): 
+        print("Existe Error con los ELEMENTOS del XML en las Siguientes Maquinas: ")
+        for x in ListaElemetosError:
+            print(f"Maquina: {x}")
+
+    else:
+        for elemento in root.findall(".//listaElementos/elemento"):
+            numeroAtomico = elemento.find("numeroAtomico").text
+            simbolo = elemento.find("simbolo").text
+            nombreElemento = elemento.find("nombreElemento").text
             
-            print(f"Elemento: {elemento}")
+            print(f"Numero Atomico: {numeroAtomico}, Simbolo: {simbolo}, Nombre Elemento: {nombreElemento}")
 
 
+        for Maquina in root.findall(".//listaMaquinas/Maquina"):
+            nombre = Maquina.find("nombre").text
+            numeroPines = Maquina.find("numeroPines").text
+            numeroElementos = Maquina.find("numeroElementos").text
+            
+
+            print(f"Nombre: {nombre}, Numero Pines: {numeroPines}, Numero Elementos: {numeroElementos}")
+            contadorPines = 0
+
+            for elementos in Maquina.findall("pin/elementos/elemento"):
+                elemento = elementos.text
+                contadorPines += 1
+
+                
+                print(f"Elemento: {elemento}")
+            
 
 
-    for compuesto in root.findall(".//listaCompuestos/compuesto"):
-        nombre = compuesto.find("nombre").text
+        for compuesto in root.findall(".//listaCompuestos/compuesto"):
+            nombre = compuesto.find("nombre").text
 
 
-        for elementos in compuesto.findall("elementos/elemento"):
-            elemento = elementos.text
+            for elementos in compuesto.findall("elementos/elemento"):
+                elemento = elementos.text
 
-            print(f"Nombre: {nombre}, Elemento: {elemento}")
+                print(f"Nombre: {nombre}, Elemento: {elemento}")
 
             
 
---
+++ clasesDatos.py
@@ -0,0 +1,9 @@
+class elemento():
+
+    def __init__(self, numeroAtom, simbolo, nombreElemento):
+        self.numeroAtom = numeroAtom
+        self.simbolo = simbolo
+        self.nombreElemento = nombreElemento
+
+
+    
